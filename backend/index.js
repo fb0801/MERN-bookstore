2c46041269d3e943558519e7cf9e3550501955cc
fb0801 <fbhatti0801@gmail.com>
2023-09-08 15:18:19 +0100

    get book by id

--- backend/index.js
+++ backend/index.js
@@ -57,6 +57,20 @@ app.get('/books', async (request, response) => {
     }
 });
 
+// route for get all book from DB by ID
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message });
+    }
+});
+
 mongoose.connect(mongoDBURL)
 .then(() => {
 console.log('connected')
