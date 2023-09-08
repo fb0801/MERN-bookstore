766c6d6c4c6bc523d2c3c65302bd2d4efe6f100b
fb0801 <fbhatti0801@gmail.com>
2023-09-08 19:45:19 +0100

    update book

--- backend/index.js
+++ backend/index.js
@@ -71,6 +71,34 @@ app.get('/books/:id', async (request, response) => {
     }
 });
 
+//update a book 
+app.put('/books/:id', async (request, response) => {
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'send all required feilds: title, author, publishYear',
+            });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if(!result){
+            return response.status(404).json({message: 'Book not found'})
+        }
+        return response.status(200).send({ message: 'Book successfully updated' })
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
 mongoose.connect(mongoDBURL)
 .then(() => {
 console.log('connected')
