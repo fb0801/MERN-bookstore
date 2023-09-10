ede3b97fca064509ae26271014fbdd90e753df64
fb0801 <fbhatti0801@gmail.com>
2023-09-10 19:09:00 +0100

    show books list in react

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,13 @@ const app = express();
 app.use(express.json());
 
 //cors middleware
-//app.use(cors());
+app.use(cors());
 
-app.use(cors({
-    origin: 'https://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-}))
+//app.use(cors({
+//    origin: 'https://localhost:3000',
+//    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//    allowedHeaders: ['Content-Type']
+//}))
 
 app.get('/', (request, response) => {
     console.log(request);
